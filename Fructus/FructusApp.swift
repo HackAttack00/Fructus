486d75279fa835251b4d5830c8d80fa651d9fc87
hack_이승철 <sclee@afreecatv.com>
2023-02-26 23:55:00 +0900

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Seungchul Lee on 2023/02/26.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
