db7a1c70ee5055cf1f5fcdf575e9b4da3aea0343
Hack_이승철 <sclee@afreecatv.com>
2023-02-27 18:35:31 +0900

    tab생성 및 스크롤 되는데까지 완성

---
+++ Fructus/App/OnboardingView.swift
@@ -0,0 +1,26 @@
+//
+//  OnboardingView.swift
+//  Fructus
+//
+//  Created by Seungchul Lee on 2023/02/27.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        TabView {
+            ForEach(0..<5) { item in
+                FruitCardView()
+            }
+        }
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
